70ff0dbbb5f6b86759b9974e9b9fb0dafbcfcd61
Joshua Snider <josh@joshuasnider.com>
2015-10-03 01:04:18 -0400

    Light refactoring.

--- clonegits.py
+++ clonegits.py
@@ -8,7 +8,8 @@ from github import Github
 import os
 import subprocess
 
-def main():
+def make_args():
+  ''' Make the parser for the command line.'''
   parser = argparse.ArgumentParser(
     description="Clone all of your GitHub repos. Including private repos.");
   parser.add_argument('--user', help="Your github username")
@@ -18,7 +19,11 @@ def main():
     help="Repository to clone your repos into. (default: current directory)")
   parser.add_argument('--nopull', action='store_true',
     help="Disables updating preexisting repos. (default: false)")
-  args = parser.parse_args()
+  return parser
+
+def make_github_agent(cli_args):
+  ''' Create the Github object used
+      to access their API.'''
   g = None
   if args.token:
     g = Github(args.token)
@@ -30,10 +35,17 @@ def main():
       os.chdir(args.dest)
     passw = getpass.getpass('Password:')
     g = Github(user, passw)
+  return g
+
+def main():
+  ''' Sync a user's GitHub repos with the current machine.'''
+  parser = make_args()
+  args = parser.parse_args()
+  g = make_github_agent(args)
   user = g.get_user().login
   for repo in g.get_user().get_repos():
     if repo.full_name.startswith(user):
-      if not os.path.exists(repo.name): #Check directory doesn't exist.
+      if not os.path.exists(repo.name):
         print(repo.full_name)
         subprocess.call(["git", "clone",
                        "https://github.com/" + repo.full_name])
