c477681345d8b6e664e876c593f2117a628d705b
Joshua Snider <josh@joshuasnider.com>
2015-10-02 22:53:53 -0400

    OAuth authentication option.

--- clonegits.py
+++ clonegits.py
@@ -12,18 +12,25 @@ def main():
   parser = argparse.ArgumentParser(
     description="Clone all of your GitHub repos. Including private repos.");
   parser.add_argument('--user', help="Your github username")
+  parser.add_argument('--token',
+    help="OAuth token. Alternative to password authentication.")
   parser.add_argument('--dest',
     help="Repository to clone your repos into. (default: current directory)")
   parser.add_argument('--nopull', action='store_true',
     help="Disables updating preexisting repos. (default: false)")
   args = parser.parse_args()
-  user = args.user
-  if not user:
-    user = raw_input('User:')
-  if args.dest:
-    os.chdir(args.dest)
-  passw = getpass.getpass('Password:')
-  g = Github(user, passw)
+  g = None
+  if args.token:
+    g = Github(args.token)
+  else:
+    user = args.user
+    if not user:
+      user = raw_input('User:')
+    if args.dest:
+      os.chdir(args.dest)
+    passw = getpass.getpass('Password:')
+    g = Github(user, passw)
+  user = g.get_user().login
   for repo in g.get_user().get_repos():
     if repo.full_name.startswith(user):
       if not os.path.exists(repo.name): #Check directory doesn't exist.
