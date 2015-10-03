b319e4e2c10325fcb08619ec0322be7d3a21b272
Joshua Snider <josh@joshuasnider.com>
2015-10-02 20:14:02 -0400

    Basic version.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 A tool to clone all of a user's github repos to the local
 machine.
+
+## Usage
+
+Run `python2 clonegits.py` either run it in the target directory
+or use `--dest` to supply it with a directory to download into.
+
+## Requirements
+
+This only requires pygithub, which can be installed with
+`pip install pygithub`.
--- clonegits.py
+++ clonegits.py
@@ -1,9 +1,31 @@
+#!/usr/bin/env python2
+'''
+  @Author: Josh Snider
+'''
+import argparse
 import getpass
 from github import Github
+import os
+import subprocess
 
-user = raw_input('User:')
+parser = argparse.ArgumentParser(
+  description="Clone all of your GitHub repos. Including private repos.");
+parser.add_argument('--user', help="Your github username")
+parser.add_argument('--dest',
+  help="Repository to clone your repos into. (default: current directory)")
+args = parser.parse_args()
+user = args.user
+if not user:
+  user = raw_input('User:')
+if args.dest:
+  os.chdir(args.dest)
 passw = getpass.getpass('Password:')
 g = Github(user, passw)
 for repo in g.get_user().get_repos():
-  print(repo.full_name)
-  # TODO Clone repos
+  if repo.full_name.startswith(user):
+    if not os.path.exists(repo.name): #Check directory doesn't exist.
+      print(repo.full_name)
+      subprocess.call(["git", "clone",
+                     "https://github.com/" + repo.full_name])
+    else:
+      print("Skipping " + repo.name)
