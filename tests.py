e93af403c5ed73c1a753459632e63e1c211dc3a9
Joshua Snider <josh@joshuasnider.com>
2015-10-07 22:14:43 -0400

    Improved test cases.

--- tests.py
+++ tests.py
@@ -3,12 +3,35 @@ import subprocess
 import unittest
 
 class Tests(unittest.TestCase):
+  ''' Testcases assume that my OAuth token is present in the environment
+      as the environment variable OAuth.'''
+
+  def delete_dirs(self, path):
+    ''' Delete all directories in given path.'''
+    for target in os.listdir(path):
+      if os.path.isdir(target):
+        subprocess.call(['rm', '-rf', target])
 
   def test_basic(self):
     ''' Test we can run without crashing.'''
     token = os.environ['OAuth']
     assert len(token)
     subprocess.check_output(['python', 'clonegits.py', '--token', token])
+    self.delete_dirs(os.getcwd())
+
+  def test_public_repo(self):
+    ''' Test we can get a public repo.'''
+    token = os.environ['OAuth']
+    subprocess.check_output(['python', 'clonegits.py', '--token', token])
+    assert "CloneGits" in os.listdir(os.getcwd())
+    self.delete_dirs(os.getcwd())
+
+  def test_public_repo(self):
+    ''' Test we can get a private repo.'''
+    token = os.environ['OAuth']
+    subprocess.check_output(['python', 'clonegits.py', '--token', token])
+    assert "Resume" in os.listdir(os.getcwd())
+    self.delete_dirs(os.getcwd())
 
 if __name__ == '__main__':
   unittest.main()
