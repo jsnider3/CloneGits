78903624b80f32153e5e3d00403013de6684dbac
Joshua Snider <josh@joshuasnider.com>
2015-11-14 19:00:17 -0500

    Test ruleset matching.

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import clonegits
+from github import Github
 import os
 import subprocess
 import unittest
@@ -9,7 +11,7 @@ class Tests(unittest.TestCase):
   def delete_dirs(self, path):
     ''' Delete all directories in given path.'''
     for target in os.listdir(path):
-      if os.path.isdir(target):
+      if os.path.isdir(target) and target != '.git':
         subprocess.call(['rm', '-rf', target])
 
   def tearDown(self):
@@ -27,6 +29,43 @@ class Tests(unittest.TestCase):
     token = os.environ['OAuth']
     subprocess.check_output(['python', 'clonegits.py', '--token', token])
     assert "CloneGits" in os.listdir(os.getcwd())
+
+  def test_ruleset_fork(self):
+    ''' Test we check if the original owner matches for forks. '''
+    kwargs = {}
+    kwargs["rules_file"] = "samplerules.json"
+    rules = clonegits.Ruleset(**kwargs)
+    gh_agent = Github()
+    repo = gh_agent.get_repo('jsnider3/tarpon')
+    assert rules.get_dest(repo) == '/tmp/gits/kunal'
+
+  def test_ruleset_language(self):
+    ''' Test we check if the language matches. '''
+    kwargs = {}
+    kwargs["rules_file"] = "samplerules.json"
+    rules = clonegits.Ruleset(**kwargs)
+    gh_agent = Github()
+    repo = gh_agent.get_repo('jsnider3/Workspace')
+    assert rules.get_dest(repo) == '/tmp/gits/java'
+
+  def test_ruleset_owner(self):
+    ''' Test we check if the owner matches. '''
+    kwargs = {}
+    kwargs["rules_file"] = "samplerules.json"
+    rules = clonegits.Ruleset(**kwargs)
+    gh_agent = Github()
+    repo = gh_agent.get_repo('jsnider3/StudyNotes')
+    assert rules.get_dest(repo) == '/tmp/gits'
+
+  def test_ruleset_default(self):
+    ''' Test we work if noone matches. '''
+    kwargs = {}
+    kwargs["rules_file"] = "samplerules.json"
+    rules = clonegits.Ruleset(**kwargs)
+    gh_agent = Github()
+    repo = gh_agent.get_repo('torvalds/linux')
+    print(rules.get_dest(repo))
+    assert rules.get_dest(repo) == os.getcwd()
 
   #def test_private_repo(self):
   #  ''' Test we can get a private repo.'''
